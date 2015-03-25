c69a7909aadfd8a474be1b50fab9c184c1b9d920
racoonsmakeokayfriends <amanda.knight314@gmail.com>
2015-03-24 19:43:31 -0700

    updated the game invitation process

--- battleship-ui.js
+++ battleship-ui.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
   var $LOBBY_LIST_USERS = $('#page-lobby #present_users');
   var $LOBBY_MD_INVITATION = $('#page-lobby #md-invitation');
-
+  var $MD_WAITING = $('#md-waiting');
   var $ACCEPT_INVITE_BTN = $('#accept_invite_btn');
   var $REJECT_INVITE_BTN = $('#reject_invite_btn');
   var $CANCEL_INVITE_BTN = $('#cancel_invite_btn')
@@ -197,7 +197,7 @@ $(document).ready(function() {
 
     // check if its us&we got a invitation
     if (get_user_id(snapshot) == my_data.id && snapshot.val().status == 'invited') {
-      create_invitation(snapshot);
+      if (snapshot.val().gameroom_key){create_invitation(snapshot);}
     }
   });
 
@@ -286,102 +286,88 @@ $(document).ready(function() {
   }
 
   function start_game() {
+    open_page($PAGE_GAMEROOM);
+    $MD_WAITING.addClass('hidden');
     var battleship_ref = get_my_gameroom_ref();
     battleship_controller = new Battleship.Controller(battleship_ref,my_data.id);
-    battleship_ref.once('value',function(snap) {
-      if (!snap.val().sink_alert){
-        battleship_controller.set_ship_sink_alert(false);
-      }
-      if (snap.val().num_shots_type){
-        battleship_controller.set_num_shots(snap.val().num_shots_type);
-      }
-    })
+    // battleship_ref.child('options').once('value',function(options_snap) {
+    //   console.log('==== start: options ====')
+    //   var options_obj = options_snap.val();
+    //   console.log(options_obj.ship_sink_alert_on)
+    //   console.log(options_obj.ship_sink_alert_on)
+    //   if (!options_obj.ship_sink_alert_on){
+    //     battleship_controller.set_ship_sink_alert(false);
+    //   }
+    //   if (options_obj.num_shots_type){
+    //     battleship_controller.set_num_shots(options_obj.num_shots_type);
+    //   }
+    //   console.log('====  end : options ====')
+    // })
   }
 
   function join_game() {
-    var my_gameroom_ref = get_my_gameroom_ref();
-    my_gameroom_ref.child('player_left').set(false);
-    // see if this has been removed (by other player rejecting to replay)
-    my_gameroom_ref.child('player_left').on('value',function (snap) {
-      if (snap.val() == true) {
-        alert('other player rejected replay');
-        leave_game_room();
-        my_gameroom_ref.remove();
-      }
-    });
+    var my_gr = get_my_gameroom_ref();
 
-    my_gameroom_ref.child('user_list').child(my_data.id).set({'timestamp':Firebase.ServerValue.TIMESTAMP});
-    open_page($PAGE_GAMEROOM);
-    
+    my_gr.child('player_left').set(false);
+
+    all_users_ref.child(my_data.id).child('status').set('gameroom');
     var onlycallonce_hack = true;
 
+    // add me to the gameroom
+    my_gr.child('user_list').child(my_data.id).set({'timestamp':Firebase.ServerValue.TIMESTAMP});
+
     // === PLAYER ENTERED ===
-    my_gameroom_ref.child('user_list').on('child_added', function (snapshot) {
-      if (get_user_id(snapshot) == PLACEHOLDER_FLAG) return;
-      // add_user_to_user_list(snapshot); // we're not showing the userlist for battleship
+    my_gr.child('user_list').on('child_added', function (user_snap) {
+      if (get_user_id(user_snap) == PLACEHOLDER_FLAG) return;
+      // add_user_to_user_list(user_snap); // we're not showing the userlist for battleship
       // todo: check if the right number of players are here
-      my_gameroom_ref.child('user_list').once('value',function(s) {
+      my_gr.child('user_list').once('value',function(s) {
         // NOTE: edit this to reflect when we're ready to play
         // NOTE: the -1 is for that stupid placeholder
         if (s.numChildren()-1 == NUM_USERS&&onlycallonce_hack) {
           start_game();
           onlycallonce_hack = false;
         }
-
-        
+        else if (s.numChildren()-1 < NUM_USERS) {
+          $MD_WAITING.removeClass('hidden');
+        }        
       });
     }); 
 
-    // === PLAYER LEFT ===
-    my_gameroom_ref.child('user_list').on('child_removed', function (snapshot) {
-      // delete the entire gameroom when everyone leaves
-      // note, it needs to be 1/we have the placeholder because if we get to 0, the userlist (not gameroom) will delete itself
-      $MSG_OPPONENT_LEFT.fadeIn();
-      // todo: take player back to lobby
-      // my_gameroom_ref.child('user_list').child(my_data.id).remove();
-      leave_game_room();
-      my_gameroom_ref.child('user_list').once('value',function (snapshot) {
-        if (snapshot.numChildren() == 1) {
-          my_gameroom_ref.remove();
-        }
-      });
-    });
-
     // === MESSAGE ADDED ===
-    my_gameroom_ref.child('chatlog').on('child_added', function (snapshot) {
+    my_gr.child('chatlog').on('child_added', function (snapshot) {
       add_msg_to_chatlog(snapshot);
     });
     // === GAME OVER ===
-    my_gameroom_ref.child('gameover').on('value',function (snapshot) {
+    my_gr.child('gameover').on('value',function (snapshot) {
       if (snapshot.val() == true) {
         $('#md-replay').removeClass('hidden');
       }
     });
   }
-
   function alert_invite_rejection(my_gameroom_ref) {
     var alert_data = {'author':'SYS','timestamp':Firebase.ServerValue.TIMESTAMP};
     alert_data['message'] = 'User ' + my_data.id + ' has rejected your invitation.';
     my_gameroom_ref.child('chatlog').push(alert_data);
   }
 
-  function send_back_to_lobby() {
-    var my_gr = get_my_gameroom_ref();
+  function send_back_to_lobby(my_gr) {
     my_gr.child('player_left').set(true);
+    my_gr.child('user_list').child(my_data.id).remove();
     all_users_ref.child(my_data.id).child('status').set('lobby');
     all_users_ref.child(my_data.id).child('gameroom_key').set(null);
   }
 
   $ACCEPT_INVITE_BTN.click(function () {
-    all_users_ref.child(my_data.id).child('status').set('gameroom');
     join_game();
   });
 
   $REJECT_INVITE_BTN.click(function () {
-    send_back_to_lobby();
-    alert_invite_rejection(get_my_gameroom_ref());
+    var my_gr = get_my_gameroom_ref();
+    //alert_invite_rejection(my_gr); will need for >2 player games (i think)
+    send_back_to_lobby(my_gr);
   });
-  
+
   $EXIT_GAMEROOM_BTN.click(function() {
     leave_game_room();
   });
@@ -400,6 +386,10 @@ $(document).ready(function() {
     var my_gameroom_ref = get_my_gameroom_ref();
     var msg_data = {'message':msg,'author':my_data.id,'timestamp':Firebase.ServerValue.TIMESTAMP};
     my_gameroom_ref.child('chatlog').push(msg_data);
+  });
+
+  $CANCEL_INVITE_BTN.click(function () {
+    leave_game_room();
   });
 
 /* =========================================================
@@ -435,14 +425,17 @@ $(document).ready(function() {
   function get_my_gameroom_ref() {
     var my_user_ref = all_users_ref.child(my_data.id);
     var gameroom_key;
-    my_user_ref.once('value',function(snapshot) { gameroom_key = snapshot.val()['gameroom_key']; });
+    my_user_ref.once('value',function(snapshot) { 
+      gameroom_key = snapshot.val()['gameroom_key']; 
+    });
     return game_rooms_ref.child(gameroom_key);
   }
 
   function leave_game_room() {
     var my_gameroom_ref = get_my_gameroom_ref();
-    my_gameroom_ref.child('user_list').child(my_data.id).remove();
     my_gameroom_ref.child('player_left').set(true);
+    my_gameroom_ref.child('user_list').child(my_data.id).remove();
+    // my_gameroom_ref.child('player_left').set(true);
     all_users_ref.child(my_data.id).child('status').set('lobby');
     all_users_ref.child(my_data.id).child('gameroom_key').set(null);
     open_page($PAGE_LOBBY);    
@@ -465,14 +458,18 @@ $(document).ready(function() {
       $LOBBY_MD_INVITATION.find('.md-txt').html(txt);
     });
 
-    my_gr.child('player_left').on('value',function(snap) {
+    $LOBBY_MD_INVITATION.removeClass('hidden');
+
+
+    // see if this has been removed (by other player rejecting to replay)
+    // === PLAYER LEFT/REJECTED GAME ===
+    my_gr.child('player_left').on('value',function (snap) {
       if (snap.val() == true) {
-        $LOBBY_MD_INVITATION.addClass('hidden');
+        open_page($PAGE_LOBBY);
         $MSG_OPPONENT_CANCELLED.fadeIn();
-        send_back_to_lobby();
+        send_back_to_lobby(my_gr);
       }
     });
-    $LOBBY_MD_INVITATION.removeClass('hidden');
   }
 
 
